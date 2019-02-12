3553f8857c9032f57cca36b27d507715ede36027
spikeburton <ralphburton3@gmail.com>
2019-02-12 11:37:13 -0500

    add most_viewers to Show

--- lib/show.rb
+++ lib/show.rb
@@ -2,15 +2,23 @@ class Show < ActiveRecord::Base
   has_many :ratings
   has_many :viewers, through: :ratings
 
+  ## Average rating for this show from all viewers
   def average_rating
     (ratings.sum(:rating).to_f / num_ratings).round(1)
   end
 
+  ## Number of ratings for this show
   def num_ratings
     ratings.count
   end
 
+  ## Return the show with the highest average rating
   def self.highest_average_rating
     Show.all.max_by { |show| show.average_rating }
+  end
+
+  ## Return the show with the most number of viewers
+  def self.most_viewers
+    Show.all.max_by { |show| show.viewers.count }
   end
 end
