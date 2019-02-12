07d5fd568812122e84af2820d4d348ebd595297d
spikeburton <ralphburton3@gmail.com>
2019-02-12 11:52:30 -0500

    add favorite_show and top_three to Viewer

--- lib/viewer.rb
+++ lib/viewer.rb
@@ -1,4 +1,14 @@
 class Viewer < ActiveRecord::Base
   has_many :ratings
   has_many :shows, through: :ratings
+
+  ## Get the top rated show for this viewer
+  def favorite_show
+    ratings.order(rating: :DESC).limit(1).collect(&:show)
+  end
+
+  ## Get top three rated shows for this viewer
+  def top_three
+    ratings.order(rating: :DESC).limit(3).collect(&:show)
+  end
 end
