b405e25ec8a606b1bd1885e15b17ca83ccfef5e9
spikeburton <ralphburton3@gmail.com>
2019-02-13 13:29:27 -0500

    add functionality to generate data and save as json

---
+++ db/generate_shows.rb
@@ -0,0 +1,26 @@
+require_relative '../config/environment'
+
+shows = []
+
+while shows.size < 100
+  begin
+    data = JSON.parse(RestClient.get("http://api.tvmaze.com/shows/#{rand(1..10000)}"))
+    shows << {
+      title: data["name"],
+      network: data["network"]["name"],
+      country: data["network"]["country"]["name"]
+    }
+    puts "SUCCESS"
+  rescue RestClient::ExceptionWithResponse => e
+    puts "ERROR: #{e.response}"
+  rescue StandardError => e
+    puts "ERROR: NO DATA"
+  end
+end
+
+countries = shows.collect { |s| s[:country] }.compact.uniq
+
+File.open("db/json/shows.json", "w") { |f| f.write shows.to_json }
+File.open("db/json/countries.json", "w") { |f| f.write countries.to_json }
+
+binding.pry
