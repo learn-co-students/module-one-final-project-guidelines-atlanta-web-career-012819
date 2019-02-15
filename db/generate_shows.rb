9fd52f742aefe20ae3ec61f899485cc301906c86
spikeburton <ralphburton3@gmail.com>
2019-02-15 11:33:54 -0500

    improve error checking for data retrieval, ensuring valid and complete data

--- db/generate_shows.rb
+++ db/generate_shows.rb
@@ -2,22 +2,27 @@ require_relative '../config/environment'
 
 shows = []
 
-while shows.size < 10
+while shows.size < 100
   begin
     data = JSON.parse(RestClient.get("http://api.tvmaze.com/shows/#{rand(1..10000)}"))
 
     if data["name"] && data["network"] && data["summary"]
-      shows << {
-        title: data["name"],
-        network: data["network"]["name"],
-        country: data["network"]["country"]["name"],
-        summary: data["summary"].gsub(%r{</?[^>]+?>}, '')
-      }
-      shows.uniq!
-      puts "SUCCESS"
+      if data["network"]["country"] && !data["summary"].empty?
+        shows << {
+          title: data["name"],
+          network: data["network"]["name"],
+          country: data["network"]["country"]["name"],
+          summary: data["summary"].gsub(%r{</?[^>]+?>}, '')
+        }
+        shows.uniq!
+        puts "SUCCESS"
+      else
+        puts "ERROR: EMPTY DATA"
+        # binding.pry
+      end
     else
       puts "ERROR: INVALID DATA"
-      binding.pry
+      # binding.pry
     end
   rescue RestClient::ExceptionWithResponse => e
     err = JSON.parse(e.response)
