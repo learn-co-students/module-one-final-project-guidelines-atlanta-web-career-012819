ce2ab2afb04d3cad0c5ddaa4b016acb39f2266f3
spikeburton <ralphburton3@gmail.com>
2019-02-15 11:17:44 -0500

    improve error checking for data retrieved from api via rest-client

--- db/generate_shows.rb
+++ db/generate_shows.rb
@@ -2,22 +2,26 @@ require_relative '../config/environment'
 
 shows = []
 
-while shows.size < 10
+while shows.size < 100
   begin
     data = JSON.parse(RestClient.get("http://api.tvmaze.com/shows/#{rand(1..10000)}"))
-    shows << {
-      title: data["name"],
-      network: data["network"]["name"],
-      country: data["network"]["country"]["name"],
-      summary: data["summary"].gsub(%r{</?[^>]+?>}, '')
-    }
-    shows.uniq!
-    puts "SUCCESS"
+
+    if data["name"] && data["network"] && data["summary"]
+      shows << {
+        title: data["name"],
+        network: data["network"]["name"],
+        country: data["network"]["country"]["name"],
+        summary: data["summary"].gsub(%r{</?[^>]+?>}, '')
+      }
+      shows.uniq!
+      puts "SUCCESS"
+    else
+      puts "ERROR: INVALID DATA"
+      binding.pry
+    end
   rescue RestClient::ExceptionWithResponse => e
     err = JSON.parse(e.response)
     puts "ERROR: #{err["status"]} #{err["name"]}"
-  rescue StandardError => e
-    puts "ERROR: NO DATA"
   end
 end
 
